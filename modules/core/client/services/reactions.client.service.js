5f76a5f1e83067379b04763a39e53ef759c979ae
Joel Newman <joelnewman@Joels-MacBook-Pro.local>
2016-11-13 11:12:18 -0800

    added links and descriptions to the reactions

--- modules/core/client/services/reactions.client.service.js
+++ modules/core/client/services/reactions.client.service.js
@@ -1,10 +1,10 @@
 angular.module('core').value('reactions', [
-  { title: 'Ad Hominem', value: 'ad-hominem', icon: 'ad-hominem' },
-  { title: 'Strawman', value: 'strawman', icon: 'strawman' },
-  { title: 'Circular reasoning', value: 'circular', icon: 'circular' },
-  { title: 'False Dichotomy', value: 'dichotomy', icon: 'dichotomy' },
-  { title: "Source doesn't back claim", value: 'incorrect-source', icon: 'incorrect-source' },
-  { title: 'Low quality source', value: 'low-quality-source', icon: 'low-quality-source' },
-  { title: 'Good Point', value: 'good-point', icon: 'good-point' },
-  { title: 'Good Citation', value: 'good-citation', icon: 'good-citation' },
+  { title: 'Ad Hominem', value: 'ad-hominem', icon: 'ad-hominem', description: 'Arguments of this kind focus not on the evidence for a view but on the character of the person advancing it; they seek to discredit positions by discrediting those who hold them.', url: 'http://www.logicalfallacies.info/' },
+  { title: 'Strawman', value: 'strawman', icon: 'strawman', description: 'A straw man argument is one that misrepresents a position in order to make it appear weaker than it actually is, refutes this misrepresentation of the position, and then concludes that the real position has been refuted.', url: 'http://www.logicalfallacies.info/' },
+  { title: 'Circular reasoning', value: 'circular', icon: 'circular', description: 'An argument is circular if its conclusion is among its premises, if it assumes (either explicitly or not) what it is trying to prove. Such arguments are said to beg the question.', url: 'http://www.logicalfallacies.info/' },
+  { title: 'False Dichotomy', value: 'dichotomy', icon: 'dichotomy', description: 'This fallacy is committed when a false dilemma is presented, i.e. when someone is asked to choose between two options when there is at least one other option available.', url: 'http://www.logicalfallacies.info/' },
+  { title: "Source doesn't back claim", value: 'incorrect-source', icon: 'incorrect-source', description: 'The cited source does not show evidence to support the position it is cited for', url: 'http://libguides.mit.edu/citing' },
+  { title: 'Low quality source', value: 'low-quality-source', icon: 'low-quality-source', description: 'The cited source is unreliable or known to be biased', url: 'http://libguides.mit.edu/citing'},
+  { title: 'Good Point', value: 'good-point', icon: 'good-point', description: 'This argument is well-written, well-reasoned and contributes to healthy discussion', url: 'http://libguides.mit.edu/citing' },
+  { title: 'Good Citation', value: 'good-citation', icon: 'good-citation', description: 'The cited source is on-topic, reliable, and free from bias', url: 'http://libguides.mit.edu/citing' },
 ]);
