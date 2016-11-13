9d09fb20d8cdc3597496b13e2cdbd8f20af82e2f
Shawn Marincas <smarincas@thinaire.net>
2016-11-12 21:08:48 -0800

    added more stuff to the chat view, added hardcoded data model to chat controller

--- modules/chat/client/controllers/chat.client.controller.js
+++ modules/chat/client/controllers/chat.client.controller.js
@@ -4,7 +4,44 @@
 angular.module('chat').controller('ChatController', ['$scope', '$location', 'Authentication', 'Socket',
   function ($scope, $location, Authentication, Socket) {
     // Create a messages array
-    $scope.messages = [];
+    $scope.messages = [
+      {
+        "position": "for",
+        "title": "Popular results differ from Electoral results frequently.",
+        "body": "In the current system, the electoral college's structure means that the president elect has lost the popular vote in 2 of the last 7 elections.",
+        "sources":[{
+          "text": "Excerpted text from cited source showing statistics",
+          "url": "wwww.url-in-support.com"
+        }],
+        comments: [
+          {
+            "body": "Here is an argument that refutes this argument",
+            "sources": [{
+              "url": "wwww.rebuttalcite.org"
+            }]
+          }
+        ],
+        created: new Date()
+      },
+      {
+        "position": "against",
+        "title": "Elections need safeguards.",
+        "body": "The electoral college is an important safeguard between popular opinion and executive office.",
+        "sources":[{
+          "text": "Excerpted text from cited source showing statistics",
+          "url": "wwww.url-in-support.com"
+        }],
+        comments: [
+          {
+            "body": "Here is an argument that refutes this argument",
+            "sources": [{
+              "url": "wwww.rebuttalcite.org"
+            }]
+          }
+        ],
+        created: new Date()
+      }
+    ];
 
     // If user is not signed in then redirect back home
     if (!Authentication.user) {
