fec3a4caa561f141cc9bfa8d8b0b2fed730dcd6b
Scott Brookie <scott@scottbrookiepc3.local>
2016-11-13 10:15:29 -0800

    Adding comment counts, fixing indentation

--- modules/core/client/controllers/topic.client.controller.js
+++ modules/core/client/controllers/topic.client.controller.js
@@ -3,44 +3,44 @@
 angular.module('core').controller('TopicController', ['$scope', '$location', 'Authentication',
     function ($scope, $location, Authentication) {
         // Create a messages array
-      $scope.messages = [
-        {
-          'position': 'for',
-          'title': 'Popular results differ from Electoral results frequently.',
-          'body': "In the current system, the electoral college's structure means that the president elect has lost the popular vote in 2 of the last 7 elections.",
-          'sources':[{
-            'text': 'Excerpted text from cited source showing statistics',
-            'url': 'wwww.url-in-support.com'
-          }],
-          comments: [
+        $scope.messages = [
             {
-              'body': 'Here is an argument that refutes this argument',
-              'sources': [{
-                'url': 'wwww.rebuttalcite.org'
-              }]
-            }
-          ],
-          created: new Date()
-        },
-        {
-          'position': 'against',
-          'title': 'Elections need safeguards.',
-          'body': 'The electoral college is an important safeguard between popular opinion and executive office.',
-          'sources':[{
-            'text': 'Excerpted text from cited source showing statistics',
-            'url': 'wwww.url-in-support.com'
-          }],
-          comments: [
+                'position': 'for',
+                'title': 'Popular results differ from Electoral results frequently.',
+                'body': "In the current system, the electoral college's structure means that the president elect has lost the popular vote in 2 of the last 7 elections.",
+                'sources':[{
+                    'text': 'Excerpted text from cited source showing statistics',
+                    'url': 'wwww.url-in-support.com'
+                }],
+                comments: [
+                    {
+                        'body': 'Here is an argument that refutes this argument',
+                        'sources': [{
+                            'url': 'wwww.rebuttalcite.org'
+                        }]
+                    }
+                ],
+                created: new Date()
+            },
             {
-              'body': 'Here is an argument that refutes this argument',
-              'sources': [{
-                'url': 'wwww.rebuttalcite.org'
-              }]
+                'position': 'against',
+                'title': 'Elections need safeguards.',
+                'body': 'The electoral college is an important safeguard between popular opinion and executive office.',
+                'sources':[{
+                    'text': 'Excerpted text from cited source showing statistics',
+                    'url': 'wwww.url-in-support.com'
+                }],
+                comments: [
+                    {
+                        'body': 'Here is an argument that refutes this argument',
+                        'sources': [{
+                            'url': 'wwww.rebuttalcite.org'
+                        }]
+                    }
+                ],
+                created: new Date()
             }
-          ],
-          created: new Date()
-        }
-      ];
+        ];
 
         $scope.join = function() {
             $scope.joined = true;
@@ -86,18 +86,18 @@ angular.module('core').controller('TopicController', ['$scope', '$location', 'Au
         };
 
         // If user is not signed in then redirect back home
-      if (!Authentication.user) {
-        $location.path('/');
-      }
+        if (!Authentication.user) {
+            $location.path('/');
+        }
         // Create a controller method for sending messages
-      $scope.sendMessage = function () {
+        $scope.sendMessage = function () {
             // Create a new message object
-        var message = {
-          text: this.messageText
-        };
+            var message = {
+                text: this.messageText
+            };
 
             // Clear the message text
-        this.messageText = '';
-      };
+            this.messageText = '';
+        };
     }
 ]);
