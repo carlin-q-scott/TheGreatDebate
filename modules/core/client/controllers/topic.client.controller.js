def410455e8c066e317a40aa8b9b548cd8013a7c
Shawn Marincas <smarincas@thinaire.net>
2016-11-13 13:14:58 -0800

    putting users into messages

--- modules/core/client/controllers/topic.client.controller.js
+++ modules/core/client/controllers/topic.client.controller.js
@@ -7,45 +7,53 @@ angular.module('core').controller('TopicController',
         points: 42
       };
 
-        // Create a messages array
-        $scope.messages = [
-            {
-                'position': 'for',
-                'title': 'Popular results differ from Electoral results frequently.',
-                'body': "In the current system, the electoral college's structure means that the president elect has lost the popular vote in 2 of the last 7 elections.",
-                'sources':[{
-                    'text': "New York Times election results page",
-                    'url': "www.nytimes.com/ article/election-results-2000s"
-                }],
-                comments: [
-                    {
-                        'body': 'This also occurs when looking at state legislative regions as well.',
-                        'sources': [{
-                            'url': 'www.washingtonpost.com/ articles/gerrymandering-in-2012-election-cost-votes'
-                        }]
-                    }
-                ],
-                created: new Date()
-            },
-            {
-                'position': 'against',
-                'title': 'Elections need safeguards.',
-                'body': 'The electoral college is an important safeguard between popular opinion and executive office.',
-                'sources':[{
-                    "text": "George Washington's letter portrayed the Electoral College as protecting the electorate against disaster",
-                    "url": "en.wikipedia.org/article/george-washington-letters"
-                }],
-                comments: [
-                    {
-                        'body': 'James Madison portrayed the Electoral College differently: as a simple component in a democratic system.',
-                        'sources': [{
-                            'url': 'en.wikipedia.org/ article/james-madison-letters'
-                        }]
-                    }
-                ],
-                created: new Date()
-            }
-        ];
+      // Create a messages array
+      $scope.messages = [
+          {
+              'position': 'for',
+              'title': 'Popular results differ from Electoral results frequently.',
+              'body': "In the current system, the electoral college's structure means that the president elect has lost the popular vote in 2 of the last 7 elections.",
+              'sources':[{
+                  'text': "New York Times election results page",
+                  'url': "www.nytimes.com/ article/election-results-2000s"
+              }],
+              comments: [
+                  {
+                      'body': 'This also occurs when looking at state legislative regions as well.',
+                      'sources': [{
+                          'url': 'www.washingtonpost.com/ articles/gerrymandering-in-2012-election-cost-votes'
+                      }]
+                  }
+              ],
+              user: {
+                username: "demosthenes",
+                points: 800
+              },
+              created: new Date()
+          },
+          {
+              'position': 'against',
+              'title': 'Elections need safeguards.',
+              'body': 'The electoral college is an important safeguard between popular opinion and executive office.',
+              'sources':[{
+                  "text": "George Washington's letter portrayed the Electoral College as protecting the electorate against disaster",
+                  "url": "en.wikipedia.org/article/george-washington-letters"
+              }],
+              comments: [
+                  {
+                      'body': 'James Madison portrayed the Electoral College differently: as a simple component in a democratic system.',
+                      'sources': [{
+                          'url': 'en.wikipedia.org/ article/james-madison-letters'
+                      }]
+                  }
+              ],
+              user: {
+                username: "locke",
+                points: 700
+              },
+              created: new Date()
+          }
+      ];
 
       $scope.join = function() {
         var joinModal = $modal.open({
